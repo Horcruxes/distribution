775816700dbda7fcad254e392a03f4245f417d61
michael ding <zhiyu.ding@gmail.com>
2026-06-19 18:08:37 +1000

    wip

--- packages/addons/addon-depends/libvpx/package.mk
+++ packages/addons/addon-depends/libvpx/package.mk
@@ -2,44 +2,17 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libvpx"
-PKG_VERSION="df655cf4fb6c2a23b964544acd015cc715752830"
+PKG_VERSION="1.15.2"
+PKG_SHA256="26fcd3db88045dee380e581862a6ef106f49b74b6396ee95c2993a260b4636aa"
 PKG_LICENSE="BSD"
-PKG_SITE="https://github.com/webmproject/libvpx"
-PKG_URL="${PKG_SITE}.git"
-PKG_DEPENDS_HOST="toolchain nasm:host"
+PKG_SITE="https://www.webmproject.org"
+PKG_URL="https://github.com/webmproject/libvpx/archive/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="WebM VP8/VP9 Codec"
 
-configure_host() {
-  HOST_ARCH=$(uname -m)
-  case ${HOST_ARCH} in
-    aarch64)
-      PKG_HOST_NAME_LIBVPX="arm64-linux-gcc"
-      ;;
-    arm)
-      PKG_HOST_NAME_LIBVPX="armv7-linux-gcc"
-      ;;
-    x86_64)
-      PKG_HOST_NAME_LIBVPX="x86_64-linux-gcc"
-      ;;
-  esac
-  ${PKG_CONFIGURE_SCRIPT} --prefix=${TOOLCHAIN} \
-                        --extra-cflags="${CFLAGS}" \
-                        --as=nasm \
-                        --target=${PKG_HOST_NAME_LIBVPX} \
-                        --disable-docs \
-                        --disable-examples \
-                        --disable-shared \
-                        --disable-tools \
-                        --disable-unit-tests \
-                        --disable-vp8-decoder \
-                        --disable-vp9-decoder \
-                        --enable-ccache \
-                        --enable-pic \
-                        --enable-static \
-                        --enable-vp8 \
-                        --enable-vp9
-}
+if [ "${TARGET_ARCH}" = "x86_64" ]; then
+  PKG_DEPENDS_TARGET+=" nasm:host"
+fi
 
 configure_target() {
 
@@ -61,7 +34,7 @@ configure_target() {
                         --target=${PKG_TARGET_NAME_LIBVPX} \
                         --disable-docs \
                         --disable-examples \
-                        --enable-shared \
+                        --disable-shared \
                         --disable-tools \
                         --disable-unit-tests \
                         --disable-vp8-decoder \
@@ -71,8 +44,4 @@ configure_target() {
                         --enable-static \
                         --enable-vp8 \
                         --enable-vp9
-}
-
-post_makeinstall_target() {
-  ln -sfv libvpx.so.8.0.1 ${INSTALL}/usr/lib/libvpx.so.6
 }
